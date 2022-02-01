979850b490343a18121efa9ef24be5f2039c2d11
SLatorre14 <s.latorre@uniandes.edu.co>
2022-01-31 19:21:57 -0500

    Hola Mundo

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/modificacion.java
@@ -0,0 +1,18 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+
+import uniandes.dpoo.taller0.consola.ConsolaOlimpicos;
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class modificacion {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hola mundo!");
+
+	}
+	
+	
+}
